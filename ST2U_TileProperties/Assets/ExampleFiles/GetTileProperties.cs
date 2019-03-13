17b02692ae044aafcacede08c27e453f27204a66
Seanba <sean@seanba.com>
2019-03-13 00:04:51 -0500

    Initial drop of Tile Properties example
    Doesn't do anything too special yet.

---
+++ ST2U_TileProperties/Assets/ExampleFiles/GetTileProperties.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Tilemaps;
+using SuperTiled2Unity;
+
+public class GetTileProperties : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            var camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+            Assert.IsNotNull(camera);
+
+            var tilemap = GameObject.FindGameObjectWithTag("TerrainMap").GetComponent<Tilemap>();
+            Assert.IsNotNull(tilemap);
+
+            var worldPosition = (Vector2)camera.ScreenToWorldPoint(Input.mousePosition);
+            var cell = tilemap.WorldToCell(worldPosition);
+
+            if (tilemap.GetTile(cell) is SuperTiled2Unity.SuperTile tile)
+            {
+                if (tile.m_CustomProperties.TryGetProperty("tile-type", out CustomProperty property))
+                {
+                    Debug.LogFormat("Tile type: {0}", property.m_Value);
+                }
+            }
+        }
+    }
+}
+
---
+++ ST2U_TileProperties/Assets/ExampleFiles/Tiled/dw-tiles.tsx
@@ -0,0 +1,184 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="Tiles" tilewidth="16" tileheight="16" spacing="1" tilecount="36" columns="12">
+ <image source="dw-tiles.png" width="204" height="51"/>
+ <tile id="0">
+  <properties>
+   <property name="tile-type" value="unknown"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="tile-type" value="unknown"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="tile-type" value="unknown"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="tile-type" value="unknown"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="tile-type" value="unknown"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="tile-type" value="unknown"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="tile-type" value="unknown"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="tile-type" value="unknown"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="tile-type" value="unknown"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="tile-type" value="unknown"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="tile-type" value="unknown"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="tile-type" value="unknown"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="tile-type" value="unknown"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="tile-type" value="unknown"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="tile-type" value="Grass"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="tile-type" value="Trees"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="tile-type" value="Hill"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="tile-type" value="Mountain"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="tile-type" value="Entrance"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="tile-type" value="unknown"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="tile-type" value="Sand"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="tile-type" value="Marsh"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="tile-type" value="unknown"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="tile-type" value="unknown"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="tile-type" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="tile-type" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="tile-type" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="tile-type" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="tile-type" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="tile-type" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="tile-type" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="tile-type" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="tile-type" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="tile-type" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="tile-type" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="tile-type" value="Water"/>
+  </properties>
+ </tile>
+</tileset>
