ca069520b777584f6e7461e1d586904b8cc275f1
Seanba <sean@seanba.com>
2019-03-13 00:09:00 -0500

    GetTileProperties cleanup
    Small changes. No new functionality.

--- ST2U_TileProperties/Assets/ExampleFiles/GetTileProperties.cs
+++ ST2U_TileProperties/Assets/ExampleFiles/GetTileProperties.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.Assertions;
 using UnityEngine.Tilemaps;
 using SuperTiled2Unity;
 
@@ -13,15 +10,11 @@ public class GetTileProperties : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             var camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-            Assert.IsNotNull(camera);
-
             var tilemap = GameObject.FindGameObjectWithTag("TerrainMap").GetComponent<Tilemap>();
-            Assert.IsNotNull(tilemap);
-
             var worldPosition = (Vector2)camera.ScreenToWorldPoint(Input.mousePosition);
             var cell = tilemap.WorldToCell(worldPosition);
 
-            if (tilemap.GetTile(cell) is SuperTiled2Unity.SuperTile tile)
+            if (tilemap.GetTile(cell) is SuperTile tile)
             {
                 if (tile.m_CustomProperties.TryGetProperty("tile-type", out CustomProperty property))
                 {
